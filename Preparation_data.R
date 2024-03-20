63c51e048fbfac6aa74e6154c442fbd5210a2193
Romain Lorrilliere <lorrilliere@mnhn.fr>
2024-03-20 09:33:12 +0100

    commentaire_romain

--- Preparation_data.R
+++ Preparation_data.R
@@ -1,11 +1,14 @@
-setwd("C:/Users/quentin.petit/Documents/Git/StageOFB") 
+library(lubridate) # [RL] pour gere efficacement les dates qui sont l'un des formats de données les plus complexe
 
-# Ouverture du jeu de données: 
+
+setwd("C:/Users/quentin.petit/Documents/Git/StageOFB")
+
+# Ouverture du jeu de données:
 data <- read.csv("Data/Comptage_estuaire_2004_2024.csv", header = T, fileEncoding = "utf-8", sep = ";")
 View(data)
 str(data)
 
-### 1. Régler les problèmes liés au noms : 
+### 1. Régler les problèmes liés au noms :
 
 #  Noms colonnes (pas de point, ni d'espace, ni les accents)
 colnames(data) <- tolower(colnames(data))
@@ -13,44 +16,45 @@ colnames(data) <- gsub(" ","_",colnames(data))
 colnames(data) <- gsub("\\.","_", colnames(data))
 colnames(data) <-iconv(colnames(data), from = 'UTF-8', to = 'ASCII//TRANSLIT')
 
-# Noms des espèces: 
+# Noms des espèces:
 data[,6] <- tolower(data[,6])
 data[,6] <- gsub(" ","_",data[,6])
 data[,6] <- gsub("\\.","", data[,6])
-data[,6] <- gsub("é","e",data[,6])
+data[,6] <- gsub("é","e",data[,6]) # [RL] très bien, normalement la fonction iconv() permet de faire ça mais en tout cas c'est important de ne plus avoir d'accent.
 data[,6] <- gsub("à","a",data[,6])
 data[,6] <- gsub("'","_",data[,6])
 data[,6] <-iconv(data[,6], from = 'UTF-8', to = 'ASCII//TRANSLIT')
 
 unique(data$espece)
 
-# -> Remplacer chevalier combattant par combattant varié : 
+# -> Remplacer chevalier combattant par combattant varié :
 
 data[,6] <- gsub("chevalier_combattant","combattant_varie",data[,6])
 data[,6] <- gsub("bernache_nonette","bernache_nonnette", data[,6])
 data[,6] <- gsub("tournepierre","tournepierre_a_collier", data[,6])
 
-# Forme des dates: regarder les formats (JJ/MM/YYYY) 
+# Forme des dates: regarder les formats (JJ/MM/YYYY)
 class(data$date)
 unique(data$date)
 
-data[,1] <- gsub("13/06/22","13/06/2022",data[,1])
+data[,1] <- gsub("13/06/22","13/06/2022",data[,1]) # [RL] un peu barbar et fastidieux mais efficace. la fonction mdy() de lubridate fait ça tout seul
 data[,1] <- gsub("10/11/22","10/11/2022",data[,1])
-data[,1] <- gsub("20/01/23","20/01/2023",data[,1])                                  
-data[,1] <- gsub("17/04/23","17/04/2023",data[,1])                    
-data[,1] <- gsub("18/04/23","18/04/2023",data[,1])                   
-data[,1] <- gsub("14/09/23","14/09/2023",data[,1])                 
+data[,1] <- gsub("20/01/23","20/01/2023",data[,1])
+data[,1] <- gsub("17/04/23","17/04/2023",data[,1])
+data[,1] <- gsub("18/04/23","18/04/2023",data[,1])
+data[,1] <- gsub("14/09/23","14/09/2023",data[,1])
 data[,1] <- gsub("12/10/23","12/10/2023",data[,1])
 data[,1] <- gsub("12/12/23","12/12/2023",data[,1])
-data[,1] <- gsub("10/01/24","10/01/2024",data[,1])                 
+data[,1] <- gsub("10/01/24","10/01/2024",data[,1])
 data[,1] <- gsub("11/01/24","11/01/2024",data[,1])
 
-# - > Mettre au format DATE : 
+# - > Mettre au format DATE :
 
 data$date <- as.Date(data$date, format = "%d/%m/%Y")
+# [RL] data$date <- dmy(data$date)
 class(data$date)
-## Format date : YYYY - MM - JJ 
-data$Date <- format(data$date,"%d/%m/%Y") #Ca renvoie au format "character" 
+## Format date : YYYY - MM - JJ
+data$Date <- format(data$date,"%d/%m/%Y") #Ca renvoie au format "character" # [RL] OK mais pas nécessaire
 
 #  Nom des secteurs :
 
@@ -62,8 +66,8 @@ data[,5] <- gsub("/","", data[,5])
 data[,5] <- gsub("é","e",data[,5])
 data[,5] <-iconv(data[,5], from = 'UTF-8', to = 'ASCII//TRANSLIT')
 
-# Problèmes avec certains noms : 
- 
+# Problèmes avec certains noms :
+
 data[,5] <- gsub("paimboeuf___corsept","paimboeuf_corsept",data[,5])
 data[,5] <- gsub("paimboeuf_corsept_","paimboeuf_corsept",data[,5])
 data[,5] <- gsub("saint_brevin__mean","saint_brevin_mean",data[,5])
@@ -72,12 +76,12 @@ data[,5] <- gsub("saint_brevin__mean","saint_brevin_mean",data[,5])
 unique(data$site)
 data[,4] <- tolower(data[,4])
 
-#Remplacement des cases vides par le nom du site : 
+#Remplacement des cases vides par le nom du site :
 data$site[data$site == ""] <- "estuaire"
 
-### 2. Compiler le tableau "espece" et jeu de données + sélectionner les taxons d'intérêt :  
+### 2. Compiler le tableau "espece" et jeu de données + sélectionner les taxons d'intérêt :
 
-# Ajouter les noms latins au jeu de données : 
+# Ajouter les noms latins au jeu de données :
 
 espece <- read.csv("Data/espece.csv")
 View(espece)
@@ -96,20 +100,20 @@ espece[,5] <-iconv(espece[,5], from = 'UTF-8', to = 'ASCII//TRANSLIT')
 
 espece <- espece[-c(98),]
 
-#Fusion des deux jeux de données : 
+#Fusion des deux jeux de données :
 
 data_esp <- merge(data,espece, by.x = "espece", by.y = "french_name")
 View(data_esp)
 unique(data_esp$espece)
 
-# Selectionnner les espèces qui nous intéressent : Anatidés + Limicoles 
+# Selectionnner les espèces qui nous intéressent : Anatidés + Limicoles
 # -> Choix des Ansériformes + Charadriiformes
 
 data <- subset(data_esp, data_esp$order_tax == "Ansériformes"|data_esp$order_tax == "Charadriiformes")
 View(data)
 unique(data$espece)
 
-# Dégager les laridés + la sterne naine (Sternidés) : 
+# Dégager les laridés + la sterne naine (Sternidés) :
 
 data <- subset(data, !(data$family_tax=="Laridés"|data$family_tax=="Sternidés"))
 unique(data$espece)
@@ -118,12 +122,12 @@ unique(data$espece)
 data <- data[,c(1:14,17:18,28:30)]
 
 
-### 3. Tentative d'ajout des lignes espèces manquantes dans le jeu de données : 
+### 3. Tentative d'ajout des lignes espèces manquantes dans le jeu de données :
 
-# Création d'un ID dans data pour pouvoir ensuite "fusionner" les deux tableaux : 
+# Création d'un ID dans data pour pouvoir ensuite "fusionner" les deux tableaux :
 data$ID <- paste(data$secteur,data$date,data$espece)
 
-# Création du tableau "inventaire" à croiser avec le jeu de données Data :  
+# Création du tableau "inventaire" à croiser avec le jeu de données Data :
 
 data$ID <- paste(data$secteur,data$date)
 
@@ -131,50 +135,56 @@ ID <- unique(data$ID)
 sp <- unique(data$espece)
 
 # Construction inventaire (pour chaque ID = la liste des 53 espèces anatidés limicoles)
-inventaire <- expand.grid(ID, sp)
+inventaire <- expand.grid(ID, sp) # [RL] très bien
 View(inventaire)
 
-# Création d'un ID dans inventaire prenant en compte les espèces pour le combiner ensuite avec un ID 
-# dans les data 
+# Création d'un ID dans inventaire prenant en compte les espèces pour le combiner ensuite avec un ID
+# dans les data
 
 inventaire$ID_Sp <- paste(inventaire$Var1,inventaire$Var2)
 
-# Création de l'ID espece dans le jeu de données : 
+# Création de l'ID espece dans le jeu de données :
 
 data$ID_Sp <- paste(data$ID,data$espece)
 
-# Combinaison des deux tableaux : 
+# Combinaison des deux tableaux :
 data_F <- merge(inventaire, data, by.x = "ID_Sp", by.y = "ID_Sp", all.x = T)
 View(data_F)
 # PB -> Ajout de NA pour site, secteurs et date avec les nouvelles lignes :
 
 
-# Utilisation de cette fonction pour séparer la date et les secteurs dans l'ID : 
+# Utilisation de cette fonction pour séparer la date et les secteurs dans l'ID :
 
 library(tidyr)
-data <- separate(data_F, col = "Var1",into = c("secteur_v","date_v"),sep = " ",remove =T) 
+data <- separate(data_F, col = "Var1",into = c("secteur_v","date_v"),sep = " ",remove =T)
 help("separate")
 
-# Faire en sorte que le site "estuaire" soit renseigné dans toute la colonne : 
+# Faire en sorte que le site "estuaire" soit renseigné dans toute la colonne :
 data$site <- replace_na(data$site,"estuaire")
 
-# Remplacement des NA par des 0 
+# Remplacement des NA par des 0
 data$effectif[is.na(data$effectif)] = 0
 View(data)
 
-# On peut maintenant retirer les "anciennes" colonnes pour date, secteur et espece et ID : 
+# On peut maintenant retirer les "anciennes" colonnes pour date, secteur et espece et ID :
 
 data <- data[,-c(5,6,10,19:24)]
 
-# Var 2 -> espece : 
+# Var 2 -> espece :
 colnames(data)[names(data)== "Var2"] <- "espece"
 
-# On remet les noms latins + famille + ordre 
+# On remet les noms latins + famille + ordre
 
 data <- merge(data,espece, by.x = "espece", by.y = "french_name")
 data <- data[,-c(18:27)]
 
-# Pour poursuivre 
-# + Voir si c'est possible d'ajouter le nom des compteurs 
-# Refaire une colonne mois et années en extrayant l'information depuis la date
-# Voir pour les doubles comptages 
+## Pour poursuivre
+## + Voir si c'est possible d'ajouter le nom des compteurs
+## Refaire une colonne mois et années en extrayant l'information depuis la date
+## Voir pour les doubles comptages
+
+                                        # [RL] conseils :
+                                        # [RL] 1- créer une table site (id, nom, caratéristique,...)
+                                        # [RL] 2- créer une table inventaire (id, id_site,date, annee, mois,jour_julien,observateur,...)
+                                        # [RL] 3- tu as ta table observation (id_inventaire, espece, abondance)
+
